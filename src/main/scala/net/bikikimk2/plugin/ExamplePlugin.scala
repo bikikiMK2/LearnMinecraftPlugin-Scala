902b24376550e3c324e86c2cd56b7a3daec20a8d
Hibiki Kawashima <hibiki@bikikimk2.net>
2024-07-23 23:14:04 +0900

    Update ExamplePlugin.scala

--- src/main/scala/net/bikikimk2/plugin/ExamplePlugin.scala
+++ src/main/scala/net/bikikimk2/plugin/ExamplePlugin.scala
@@ -2,12 +2,13 @@ package net.bikikimk2.plugin
 import org.bukkit.plugin.java.JavaPlugin
 
 class ExamplePlugin extends JavaPlugin {
+
   override def onEnable(): Unit = {
     super.onEnable()
     getLogger.info("StartPlugin")
   }
 
-  override def onDisable(): Unit ={
+  override def onDisable(): Unit = {
     super.onDisable()
     getLogger.info("StopPlugin")
   }
