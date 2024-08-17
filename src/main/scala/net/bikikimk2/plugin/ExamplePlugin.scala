9157b63e397abb87f5ffb624971209b45dac2887
bikikiMK2 <bikiki.bikiki.9180@gmail.com>
2024-08-18 06:30:33 +0900

    Update ExamplePlugin.scala

--- src/main/scala/net/bikikimk2/plugin/ExamplePlugin.scala
+++ src/main/scala/net/bikikimk2/plugin/ExamplePlugin.scala
@@ -1,16 +1,23 @@
 package net.bikikimk2.plugin
+import org.bukkit.command.{Command, CommandSender}
 import org.bukkit.plugin.java.JavaPlugin
 
 class ExamplePlugin extends JavaPlugin {
-
+  
   override def onEnable(): Unit = {
     super.onEnable()
     getLogger.info("StartPlugin")
   }
-
+  
   override def onDisable(): Unit = {
     super.onDisable()
     getLogger.info("StopPlugin")
   }
-
+  
+  override def onCommand(sender: CommandSender, command: Command, label: String, args: Array[String]): Boolean = {
+    super.onCommand(sender, command, label, args)
+    sender.sendMessage("aaa")
+    true
+  }
+  
 }
